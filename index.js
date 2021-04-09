e4e1ad33783dfc386d9bc477f4ecd1a9b564fdbc
Nicolai <72386426+Nicolai-boiko@users.noreply.github.com>
2021-04-09 14:50:38 +0300

    fewfewf

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 const swiper = new Swiper('.channel-slider', {
     // Optional parameters
     loop: true,
-    slidesPerView: 6,
+    slidesPerView: 1,
+    breakpoints: {
+      1900: {
+        slidesPerView: 6,
+      },
+      1600: {
+        slidesPerView: 5,
+      },
+      1400: {
+        slidesPerView: 4,
+      },
+      1200: {
+        slidesPerView: 3,
+      },
+      900: {
+        slidesPerView: 2,
+      },
+      650: {
+        slidesPerView: 1,
+      },
+    },
   
     // Navigation arrows
     navigation: {
@@ -13,7 +33,18 @@ const swiper = new Swiper('.channel-slider', {
 const swiper2 = new Swiper('.recommended-slider', {
 // Optional parameters
 loop: true,
-slidesPerView: 3,
+slidesPerView: 1,
+    breakpoints: {
+      1500: {
+        slidesPerView: 3,
+      },
+      950: {
+        slidesPerView: 2,
+      },
+      760: {
+        slidesPerView: 1,
+      },
+    },
 
 // Navigation arrows
 navigation: {
@@ -25,12 +56,36 @@ navigation: {
 const swiper3 = new Swiper('.food_drink-slider', {
     // Optional parameters
     loop: true,
-    slidesPerView: 6,
-    
+    slidesPerView: 1,
+    breakpoints: {
+      1900: {
+        slidesPerView: 6,
+      },
+      1600: {
+        slidesPerView: 5,
+      },
+      1400: {
+        slidesPerView: 4,
+      },
+      1200: {
+        slidesPerView: 3,
+      },
+      900: {
+        slidesPerView: 2,
+      },
+      650: {
+        slidesPerView: 1,
+      },
+    },
     // Navigation arrows
     navigation: {
         nextEl: '.food_drink-button-next',
         prevEl: '.food_drink-button-prev',
     },
     });
-  
+
+const searchButton = document.querySelector('.mobile-search');
+const mobileSearch = document.querySelector('.input-group');
+searchButton.addEventListener('click', () => {
+  mobileSearch.classList.toggle('is-open')
+});
